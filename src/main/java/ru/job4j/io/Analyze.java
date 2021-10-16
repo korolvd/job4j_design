fd091d8c4c3561a52b82c7b70706769a0a3b2b88
korolvd <ffirin@mail.ru>
2021-10-16 18:25:29 +0500

    3.0. Тестирование IO [#173905]

--- src/main/java/ru/job4j/io/Analyze.java
+++ src/main/java/ru/job4j/io/Analyze.java
@@ -4,20 +4,15 @@ import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Analyze {
 
     public void unavailable(String source, String target) {
-        List<String[]> lines = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            lines = in.lines().map(l -> l.split(" ")).collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            List<String[]> lines = in.lines().map(l -> l.split(" ")).collect(Collectors.toList());
             boolean status = true;
             for (String[] pair : lines) {
                 if (Integer.parseInt(pair[0]) > 300) {
@@ -30,16 +25,10 @@ public class Analyze {
                         out.println(pair[1] + ";");
                         status = true;
                     }
-
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) {
-        Analyze analyze = new Analyze();
-        analyze.unavailable("log2.txt", "unavailable.csv");
     }
 }
