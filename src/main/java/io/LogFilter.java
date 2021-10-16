3ca9fecfbed1b1e7de0b032ab02ff694f92c960c
korolvd <ffirin@mail.ru>
2021-10-16 13:26:23 +0500

    0.4. BufferedOutputStream [#252490]

--- src/main/java/io/LogFilter.java
+++ src/main/java/io/LogFilter.java
@@ -1,7 +1,6 @@
 package io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,7 +8,7 @@ import java.util.stream.Collectors;
 public class LogFilter {
     public static List<String> filter(String file) {
         List<String> rsl = new ArrayList<>();
-        try (BufferedReader in  = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             rsl = in.lines().filter(l -> l.contains("404")).collect(Collectors.toList());
         } catch (Exception e) {
             e.printStackTrace();
@@ -17,8 +16,16 @@ public class LogFilter {
         return rsl;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+            log.forEach(out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        System.out.println(log);
+        save(log, "404.txt");
     }
 }
