6fefff0bd92f1f33cea3f764c70e5a56a1d88a21
korolvd <ffirin@mail.ru>
2021-10-18 21:36:21 +0500

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,26 +1,29 @@
 package ru.job4j.io;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write(("HTTP/1.1 200 OK" + System.lineSeparator() + System.lineSeparator()).getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        String[] values = str.split("=");
-                        if ("?msg".equals(values[0])) {
-                            if ("Buy".equals(values[1])) {
-                                server.isClosed();
+                        String[] values = str.split(" ");
+                        if ("GET".equals(values[0])) {
+                            switch (values[1]) {
+                                case "/?msg=Exit" -> server.close();
+                                case "/?msg=Hello" -> out.write("Hello\r\n\r\n".getBytes());
+                                default -> out.write("What\r\n\r\n".getBytes());
                             }
                         }
                     }
