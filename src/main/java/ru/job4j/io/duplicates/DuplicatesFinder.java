efedea4837dae6e02f8f00955f5f58a66c416669
korolvd <ffirin@mail.ru>
2021-10-19 08:52:38 +0500

    4.2. Поиск дубликатов [#315066 #216968]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -5,16 +5,26 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class DuplicatesFinder {
-    public static void main(String[] args) throws Exception {
+
+    public String getPath(String[] args) {
         String path = args.length == 0 ? "./" : args[0];
         if (!Path.of(path).toFile().isDirectory()) {
             throw new IllegalArgumentException("Directory " + path + " not found");
         }
-        DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
-        Files.walkFileTree(Path.of(path), duplicatesVisitor);
-        List<Path> duplicates = duplicatesVisitor.getDuplicates();
+        return path;
+    }
+
+    public void printConsole(List<Path> duplicates) {
         for (Path file : duplicates) {
             System.out.println(file);
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        DuplicatesFinder finder = new DuplicatesFinder();
+        DuplicatesVisitor duplicates = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of(finder.getPath(args)), duplicates);
+        finder.printConsole(duplicates.getDuplicates());
+
     }
 }
