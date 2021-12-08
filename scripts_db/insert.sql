552cc51fe91305dedd00809ad5c196e8837b8e5a
korolvd <ffirin@mail.ru>
2021-12-08 18:13:57 +0500

    1. Схема прав пользователи и роли. [#1093]

---
+++ scripts_db/insert.sql
@@ -0,0 +1,19 @@
+insert into role(name) values('guest');
+insert into role(name) values('user');
+insert into role(name) values('moderator');
+insert into role(name) values('admin');
+
+insert into rules(name) values('read');
+insert into rules(name) values('create');
+insert into rules(name) values('edit');
+insert into rules(name) values('delete');
+insert into rules(name) values('comment');
+
+insert into category(name) values('regular');
+insert into category(name) values('urgent');
+insert into category(name) values('important');
+
+insert into state(name) values('new');
+insert into state(name) values('in progress');
+insert into state(name) values('done');
+insert into state(name) values('close');
