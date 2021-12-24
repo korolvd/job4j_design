9af164253eb76b25d143140677c29cafee9b09fe
korolvd <ffirin@mail.ru>
2021-12-24 19:12:18 +0500

    2. Анализ доступности сервера. [#859 #216781]

--- src/main/java/ru/job4j/io/Analyze.java
+++ src/main/java/ru/job4j/io/Analyze.java
@@ -4,16 +4,15 @@ import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.PrintWriter;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Analyze {
     public void unavailable(String source, String target) {
         try (BufferedReader in = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            List<String[]> lines = in.lines().map(l -> l.split(" ")).collect(Collectors.toList());
             boolean status = true;
-            for (String[] pair : lines) {
+            String[] pair;
+            while (in.ready()) {
+                pair = in.readLine().split("\\s");
                 if (Integer.parseInt(pair[0]) > 300) {
                     if (status) {
                         out.print(pair[1] + ";");
